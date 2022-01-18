f19aaeb5b6393ea2f999022e8be6e3f92f1d1db4
Kristina Menshikova <Kristen492@mail.ru>
2022-01-18 10:33:44 +0300

    Changes

--- src/main/java/aop/TimesAspect.java
+++ src/main/java/aop/TimesAspect.java
@@ -5,12 +5,14 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
 
 @Aspect
 @Component
+@EnableAspectJAutoProxy
 public class TimesAspect {
 
     @Pointcut("@annotation(aop.Times)")
